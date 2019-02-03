fc4d1a9d2d71d7c8aa04da6a9064cae5d6e1b38d
Hipnox1 <lukcorreia@hotmail.com>
2019-02-03 20:18:17 -0200

    Add files via upload

---
+++ ServoMQTT.ino
@@ -0,0 +1,123 @@
+// Loading the ESP8266WiFi library and the PubSubClient library
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <Servo.h>
+Servo myservo;
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "YOURSSID";
+const char* password = "YOURPASSWORD";
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "YOURIPADRESS";
+const char* mqttUser = "YOURMQTTUSER";
+const char* mqttPassword = "YOURMQTTPASSWORD";
+
+// Initializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Connect an LED to each GPIO of your ESP8266
+const int ledGPIO2 = 2;
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(char* topic, byte* payload, unsigned int length) {
+   String string;
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+   // Serial.print((char)payload[i]);
+     string+=((char)payload[i]);  
+  }
+  
+    Serial.print(string);
+
+if (topic ="servo")   
+    Serial.print(" ");
+   int resultado = string.toInt();   
+   int pos = map(resultado, 1, 100, 0, 180);
+   Serial.println(pos);
+    myservo.write(pos);
+    delay(15); 
+   
+ }
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+      
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe("servo");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+ 
+  Serial.begin(115200);
+   myservo.attach(2);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+// For this project, you don't need to change anything in the loop function. 
+// Basically it ensures that you ESP is connected to your broker
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+     /*
+     YOU  NEED TO CHANGE THIS NEXT LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+     To change the ESP device ID, you will have to give a unique name to the ESP8266.
+     Here's how it looks like now:
+       client.connect("ESP8266Client");
+     If you want more devices connected to the MQTT broker, you can do it like this:
+       client.connect("ESPOffice");
+     Then, for the other ESP:
+       client.connect("ESPGarage");
+      That should solve your MQTT multiple connections problem
+
+     THE SECTION IN recionnect() function should match your device name
+    */
+    client.connect("ESP8266Client");
+}
